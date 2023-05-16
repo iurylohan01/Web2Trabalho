1eba0a9d64beff6e3f947338ec8968cd93ebb19e
iurylohan01 <iurylohan8@gmail.com>
2023-05-15 22:35:12 -0300

    Alpha 0.1.2

--- js/galeria-slider.js
+++ js/galeria-slider.js
@@ -1,40 +1,25 @@
-function slide(direction) {
-  let slidesToShow = 4;
-  let slidesToScroll = 1;
-  const sliderTrack = document.querySelector(".slider-track");
-  const sliderItem = document.querySelectorAll(".slider-item");
-  const sliderItemWidth = sliderItem[0].offsetWidth;
-  const sliderContainerWidth = document.querySelector(".slider-container").offsetWidth;
-  const sliderTrackWidth = sliderItemWidth * sliderItem.length;
+const carousel = document.querySelector('.carousel');
+const books = document.querySelectorAll('.book');
+const leftArrow = document.querySelector('.arrow.left');
+const rightArrow = document.querySelector('.arrow.right');
 
-  if (sliderContainerWidth >= 768 && sliderContainerWidth < 1024) {
-    slidesToShow = 3;
-  } else if (sliderContainerWidth >= 576 && sliderContainerWidth < 768) {
-    slidesToShow = 2;
-  } else if (sliderContainerWidth < 576) {
-    slidesToShow = 1;
-  }
+let currentIndex = 0;
+const bookWidth = books[0].offsetWidth + 10;
 
-  const movePosition = slidesToScroll * sliderItemWidth;
-  const maxPosition = -(sliderTrackWidth - slidesToShow * sliderItemWidth);
-
-  if (direction === "next" && currentIndex < sliderItem.length - slidesToShow) {
-    currentIndex += slidesToScroll;
-  } else if (direction === "prev" && currentIndex > 0) {
-    currentIndex -= slidesToScroll;
-  }
-
-  if (currentIndex === 0) {
-    document.querySelector(".slider-arrow-left").classList.add("hidden");
-  } else {
-    document.querySelector(".slider-arrow-left").classList.remove("hidden");
-  }
-
-  if (currentIndex >= sliderItem.length - slidesToShow) {
-    document.querySelector(".slider-arrow-right").classList.add("hidden");
-  } else {
-    document.querySelector(".slider-arrow-right").classList.remove("hidden");
-  }
-
-  sliderTrack.style.transform = `translateX(${currentIndex * -movePosition}px)`;
+function moveCarousel() {
+  carousel.style.transform = `translateX(-${currentIndex * bookWidth}px)`;
 }
+
+leftArrow.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    moveCarousel();
+  }
+});
+
+rightArrow.addEventListener('click', () => {
+  if (currentIndex < books.length - 7) {
+    currentIndex++;
+    moveCarousel();
+  }
+});
